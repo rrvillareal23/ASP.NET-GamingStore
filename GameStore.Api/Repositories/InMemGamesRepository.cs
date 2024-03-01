3fa5067c7b26e1ac942e76ee2129249830ecbc70
rrvillareal23 <rrvillareal@arizona.edu.com>
2024-03-01 14:29:27 -0800

    Created IGamesRepository and moved it to it's own file

--- GameStore.Api/Repositories/InMemGamesRepository.cs
+++ GameStore.Api/Repositories/InMemGamesRepository.cs
@@ -1,8 +1,7 @@
 using GameStore.Api.Entities;
-
 namespace GameStore.Api.Repositories;
 
-public class InMemGamesRepository
+public class InMemGamesRepository : IGamesRepository
 {
     private readonly List<Game> games = new()
     {
